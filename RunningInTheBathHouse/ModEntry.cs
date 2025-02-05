36d3a6efb5e1fed5ed43f06454745589484a706e
Cara Lisy <lisyce@gmail.com>
2025-02-05 00:15:36 -0800

    bath house runninggg

---
+++ RunningInTheBathHouse/ModEntry.cs
@@ -0,0 +1,26 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using HarmonyLib;
+
+namespace YourProjectName
+{
+    internal sealed class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            Harmony harmony = new(ModManifest.UniqueID);
+            harmony.Patch(
+                original: AccessTools.Method(typeof(Farmer), nameof(Farmer.setRunning)),
+                postfix: new HarmonyMethod(typeof(ModEntry), nameof(setRunning_Postfix))
+                );
+        }
+        public static void setRunning_Postfix(ref Farmer __instance)
+        {
+            if (__instance.bathingClothes.Value)
+            {
+                __instance.speed = 5;
+            }
+        }
+    }    
+}
