6d10fe08aa9ef119eddccda50a895282a63fdc7f
Cara Lisy <lisyce@gmail.com>
2025-03-25 21:51:13 -0700

    remove schemes

--- ModListPageGen/ModEntry.cs
+++ ModListPageGen/ModEntry.cs
@@ -9,16 +9,6 @@ public class ModEntry : Mod
 {
     private static readonly string CSS = "https://cdn.jsdelivr.net/gh/lisyce/StardewValleyMods@main/ModListPageGen/style.min.css";
     private static readonly string JS = "https://cdn.jsdelivr.net/gh/lisyce/StardewValleyMods@main/ModListPageGen/script.min.js";
-
-    private static readonly Dictionary<string, string> ColorSchemes = new()
-    {
-        { "pastel-red", "https://cdn.jsdelivr.net/gh/lisyce/StardewValleyMods@main/ModListPageGen/CssThemes/pastel-red.min.css" },
-        { "pastel-orange", "https://cdn.jsdelivr.net/gh/lisyce/StardewValleyMods@main/ModListPageGen/CssThemes/pastel-orange.css" },
-        { "pastel-yellow", "" },
-        { "pastel-green", "" },
-        { "pastel-blue", "" },
-        { "pastel-purple", "" }
-    };
     
     private static readonly Regex NexusIdRegex = new(@"\d+");
         
@@ -75,13 +65,6 @@ public class ModEntry : Mod
         }
         var title = args[0];
         var author = args[1];
-
-        string? colorSchemeUrl = null;
-        if (!ArgUtility.TryGet(args, 2, out var colorScheme, out var error, allowBlank: true) || !ColorSchemes.TryGetValue(colorScheme, out colorSchemeUrl))
-        {
-            Monitor.Log("Invalid color scheme. Choices are: " + string.Join(", ", ColorSchemes.Keys), LogLevel.Error);
-            return;
-        }
         
         var client = new NexusApiClient(Monitor);
 
@@ -109,8 +92,7 @@ public class ModEntry : Mod
                 .ToList(),
             DependencyTree = GetDependencyTree(Helper.ModRegistry.GetAll()),
             MainCssCDNLink = CSS,
-            MainJsCDNLink = JS,
-            ColorSchemeUrl = colorSchemeUrl,
+            MainJsCDNLink = JS
         };
         var templated = template(data);
         
