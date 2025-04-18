f5430aa5c2cdecdf0967a3eb6f25ccf76a263851
Cara Lisy <lisyce@gmail.com>
2025-04-17 19:28:37 -0700

    can refill can at well

---
+++ Functional Wishing Well/ModEntry.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using StardewModdingAPI;
+using StardewValley;
+using SObject = StardewValley.Object;
+
+namespace Functional_Wishing_Well;
+
+public class ModEntry : Mod
+{
+    public override void Entry(IModHelper helper)
+    {
+        var harmony = new Harmony(ModManifest.UniqueID);
+        harmony.Patch(
+            original: AccessTools.Method(typeof(GameLocation), nameof(GameLocation.CanRefillWateringCanOnTile)),
+            postfix: new HarmonyMethod(typeof(ModEntry), nameof(CanRefillWateringCanOnTile_Postfix)));
+    }
+
+    private static void CanRefillWateringCanOnTile_Postfix(GameLocation __instance, int tileX, int tileY, ref bool __result)
+    {
+        if (__result) return;
+        
+        SObject? obj = __instance.getObjectAtTile(tileX, tileY);
+        __result = obj is { QualifiedItemId: "(BC)BarleyZP.FunctionalWishingWell_WishingWell", isTemporarilyInvisible: false };
+    }
+}
