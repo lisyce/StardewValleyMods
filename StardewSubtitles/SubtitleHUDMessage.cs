f05145801cef385d784441881614d05b72e1b6d4
Cara Lisy <lisyce@gmail.com>
2025-04-24 18:32:23 -0700

    font size scaling

--- StardewSubtitles/SubtitleHUDMessage.cs
+++ StardewSubtitles/SubtitleHUDMessage.cs
@@ -11,6 +11,7 @@ public class SubtitleHUDMessage
     private const float DefaultTime = 3500f;
     private List<SubtitleHUDMessageElement> _subtitles;
     private SpriteFont _font;
+    private float _fontScaling;
 
     private static SubtitleHUDMessage? _instance = null;
 
@@ -18,15 +19,16 @@ public class SubtitleHUDMessage
     {
         get
         {
-            _instance ??= new SubtitleHUDMessage(Game1.smallFont);
+            _instance ??= new SubtitleHUDMessage(Game1.smallFont, 0.75f);
             return _instance;
         }
     }
 
-    private SubtitleHUDMessage(SpriteFont font)
+    private SubtitleHUDMessage(SpriteFont font, float fontScaling)
     {
         _subtitles = new List<SubtitleHUDMessageElement>();
         _font = font;
+        _fontScaling = fontScaling;
         
         AddSubtitle("Test Subtitle");
         AddSubtitle("Test Subtitle2");
@@ -46,14 +48,14 @@ public class SubtitleHUDMessage
 
     public void Draw(SpriteBatch b)
     {
-        var elHeight = (int) _font.MeasureString("Ing!").Y;
-        var elPadding = 8;
+        var elHeight = (int) (_font.MeasureString("Ing!").Y * _fontScaling);
+        var elPadding = (int) (8 * _fontScaling);
         var mainPadding = 16;
 
-        var height = 2 * mainPadding - elPadding;  // we trim off the padding from the last element
+        var height = 2f * mainPadding - elPadding;  // we trim off the padding from the last element
         foreach (var sub in _subtitles)
         {
-            height += (int)_font.MeasureString(sub.Message).Y + elPadding;
+            height += (_font.MeasureString(sub.Message).Y * _fontScaling) + elPadding;
         }
         
         var minHeight = (2 * mainPadding) + elHeight;
@@ -63,7 +65,7 @@ public class SubtitleHUDMessage
         var y = safeArea.Top + 16;
 
         var boxSourceRect = new Rectangle(301, 288, 15, 15);
-        IClickableMenu.drawTextureBox(b, Game1.mouseCursors, boxSourceRect, x, y, 300, Math.Max(height, minHeight), Color.White, drawShadow: false, scale: 4f);
+        IClickableMenu.drawTextureBox(b, Game1.mouseCursors, boxSourceRect, x, y, 300, Math.Max((int) height, minHeight), Color.White, drawShadow: false, scale: 4f);
         
         
         // draw the actual subtitles
@@ -71,7 +73,7 @@ public class SubtitleHUDMessage
         foreach (var sub in _subtitles)
         {
             var pos = new Vector2(x + mainPadding, y);
-            b.DrawString(_font, sub.Message, pos, Color.White * sub.Transparency);
+            b.DrawString(_font, sub.Message, pos, Color.White * sub.Transparency, 0, Vector2.Zero, _fontScaling, SpriteEffects.None, 1f);
             y += elHeight + elPadding;
         }
     }
