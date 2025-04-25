0f22b4388a4a927f49d4c70a7602f9f81af37fd6
Cara Lisy <lisyce@gmail.com>
2025-04-24 18:11:28 -0700

    switch to chatbox texture

--- StardewSubtitles/SubtitleHUDMessage.cs
+++ StardewSubtitles/SubtitleHUDMessage.cs
@@ -29,6 +29,8 @@ public class SubtitleHUDMessage
         _font = font;
         
         AddSubtitle("Test Subtitle");
+        AddSubtitle("Test Subtitle2");
+        AddSubtitle("Test Subtitle3");
     }
 
     public void AddSubtitle(string message)
@@ -47,22 +49,29 @@ public class SubtitleHUDMessage
         var elHeight = (int) _font.MeasureString("Ing!").Y;
         var elPadding = 8;
         var mainPadding = 16;
-        var height = (elHeight * _subtitles.Count) + (2 * mainPadding) + (elPadding * (_subtitles.Count - 1));
+
+        var height = 2 * mainPadding - elPadding;  // we trim off the padding from the last element
+        foreach (var sub in _subtitles)
+        {
+            height += (int)_font.MeasureString(sub.Message).Y + elPadding;
+        }
+        
         var minHeight = (2 * mainPadding) + elHeight;
 
         var safeArea = Utility.getSafeArea();
         var x = safeArea.Left + 16;
         var y = safeArea.Top + 16;
 
-        var boxSourceRect = new Rectangle(0, 256, 60, 60);
-        IClickableMenu.drawTextureBox(b, Game1.menuTexture, boxSourceRect, x, y, 300, Math.Max(height, minHeight), Color.White);
+        var boxSourceRect = new Rectangle(301, 288, 15, 15);
+        IClickableMenu.drawTextureBox(b, Game1.mouseCursors, boxSourceRect, x, y, 300, Math.Max(height, minHeight), Color.White, drawShadow: false, scale: 4f);
+        
         
         // draw the actual subtitles
         y += mainPadding;
         foreach (var sub in _subtitles)
         {
             var pos = new Vector2(x + mainPadding, y);
-            b.DrawString(_font, sub.Message, pos, Game1.textColor * sub.Transparency);
+            b.DrawString(_font, sub.Message, pos, Color.White * sub.Transparency);
             y += elHeight + elPadding;
         }
     }
